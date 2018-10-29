bbf470cdf748136390b3dc51756d8ab9b521ca56
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 22:24:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,47 @@ const client = new Discord.Client();
 console.log("Scrpit By ESMAEL");
 
 
+client.on("ready", () => {
+let channel =     client.channels.get("502954490975223848")
+setInterval(function() {
+channel.send(`فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي `);
+}, 25)
+})
+
+
+
+
+
+
+
+
+client.on("ready", () => {
+    client.user.setPresence({
+      status: 'dnd',
+      game: { 
+         type: 0,
+         name: '!spam  ',
+         details: `I'm : صانع البوت  ♛ 『DE』 NovritschPro#6808  `,
+         url: 'http://twitch.tv/Streammingg',
+         state: `اذا تريد تشتري بوت مثل ذا راسل  ♛ 『DE』 NovritschPro#6808 `,
+        application_id: '281376075802476544',
+         assets: {
+            small_image: `366835431037337600`,
+            small_text: ' Take This ! ' ,
+            large_image: `370451271133429760`,
+            large_text: `♥` }
+    
+      }
+        });
+    });
+    
+
+
+
+
+
+
+
 
         var prefix = "$";
     client.on('message', message => {
@@ -14,8 +55,8 @@ console.log("Scrpit By ESMAEL");
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`áíÓ áÏíß ÕáÇÍíÇÊ`**');
+  if (command == "spam") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('لازم انت ادمن      تريد بوت مثل ذا راسل     ♛ 『DE』 NovritschPro#6808  ');
 
 
    message.channel.sendMessage(args.join("  "))
@@ -23,7 +64,6 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
   }
  });
 
-
-
-
+ 
+ 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
